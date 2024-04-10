eb54e07ca3cd92f58d954ae8e73d51da0495cfb1
ashwinijayaraman <ashwinijai@gmail.com>
2024-04-11 00:26:38 +0530

    Scheduler with CRUD

--- src/main/java/org/example/service/SchedulerService.java
+++ src/main/java/org/example/service/SchedulerService.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.entity.SchedulerTest;
 import org.example.repo.SchedulerTestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
